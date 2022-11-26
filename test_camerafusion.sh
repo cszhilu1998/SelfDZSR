8626aea81f603141c7a5f1520cca0a25e1ac4b51
XDMickeyYau <yaurf44@gmail.com>
2022-11-26 22:37:06 +0000

    CameraFusionTest

--- test_camerafusion.sh
+++ test_camerafusion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Start to test the model...."
 
-dataroot="/Data/dataset/RealSR/RealSR_patches"
+dataroot="./"
 name="camerafusion_l1" # You can replace 'camerafusion_l1' with 'camerafusion_l1sw' when testing the model trained by SW loss.
 scale='2'
 data='cam'
